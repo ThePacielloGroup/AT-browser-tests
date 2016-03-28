fbf4f5fe3d05ef90e2a42ad6b906ed0e06fc624f
David Storey <storey.david@gmail.com>
2016-03-28 03:02:30 -0700

    Match style of other JS, and avoid redeclaring badge each time

--- scripts/test-scripts.js
+++ scripts/test-scripts.js
@@ -7,35 +7,35 @@
 var displaySupportResults = function() {
 
 	// This ID must be on the criteria list
-	var criteriaList = document.getElementById('criteria-list');
-	var badge;
-	var successBadge = '<span class="badge-success">Supported</span>';
-	var failureBadge = '<span class="badge-failure">Not supported</span>';
-	var manualBadge = '<span class="badge-manual">(Manually test)</span>';
+	var criteriaList = document.getElementById("criteria-list"),
+		badge = null,
+		successBadge = '<span class="badge-success">Supported</span>',
+		failureBadge = '<span class="badge-failure">Not supported</span>',
+		manualBadge = '<span class="badge-manual">(Manually test)</span>';
 
 	// Look through criteria list items to get appropriate badges
 	if (criteriaList && criteriaList.hasChildNodes) {
 		var criteriaItems = criteriaList.childNodes;
 		for (var i = 0; i < criteriaItems.length; i++) {
-			if (criteriaItems[i].nodeName === 'LI') {
+			if (criteriaItems[i].nodeName === "LI") {
 
 				// Browser support criteria check
-				if (criteriaItems[i].id === 'criteria-support') {
-					criteriaItems[i].className += ' has-badge';
+				if (criteriaItems[i].id === "criteria-support") {
+					criteriaItems[i].className += " has-badge";
 
 					if (isSupported === true) {
-						var badge = successBadge;
+						badge = successBadge;
 					} else {
-						var badge = failureBadge;
+						badge = failureBadge;
 					}
 
 				// Manually tested items
 				} else {
-					var badge = manualBadge;
+					badge = manualBadge;
 				}
 
 				// Add the appropriate badge to the list item
-				criteriaItems[i].innerHTML = '<span>' + criteriaItems[i].innerHTML + '</span> ' + badge;
+				criteriaItems[i].innerHTML = "<span>" + criteriaItems[i].innerHTML + "</span> " + badge;
 
 			}
 		}
